befa4a55d7bbc0a8d1ccd97b6faf9f8b2169c946
Bartosz Milewski <bartosz.milewski@unity.pl>
2017-11-15 15:20:16 +0100

    initial commit

---
+++ src/main/java/com/spring/bmilewsk/BmilewskApplication.java
@@ -0,0 +1,12 @@
+package com.spring.bmilewsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmilewskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmilewskApplication.class, args);
+	}
+}
